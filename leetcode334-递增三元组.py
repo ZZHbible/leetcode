199d78793fcf82388c033ae13bb11eaf2c3f2188
804459116@qq.com <804459116@qq.com>
2022-01-13 00:31:47 +0800

    leetcode334

--- leetcode334-递增三元组.py
+++ leetcode334-递增三元组.py
@@ -3,12 +3,12 @@ from typing import List
 
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        small,mid=pow(2,31),pow(2,31)
+        small, mid = pow(2, 31), pow(2, 31)
         for num in nums:
-            if num <= small :
-                small = num 
+            if num <= small:
+                small = num
             elif num <= mid:
-                mid=num
+                mid = num
             else:
                 return True
         return False
