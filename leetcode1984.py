b63a3daeffae0e5b0fe337a6f58efcbd7556ac53
zhangzhenhua <804459116@qq.com>
2022-02-11 10:01:30 +0800

    leetcode1984

--- leetcode1984.py
+++ leetcode1984.py
@@ -4,11 +4,12 @@ from typing import List
 class Solution:
     def minimumDifference(self, nums: List[int], k: int) -> int:
         nums.sort()
-        ans=1e6
-        for i in range(len(nums)-k+1):
-            if nums[i+k-1]-nums[i]<ans:
-                ans=nums[i+k-1]-nums[i]
+        ans = 1e6
+        for i in range(len(nums) - k + 1):
+            if nums[i + k - 1] - nums[i] < ans:
+                ans = nums[i + k - 1] - nums[i]
         return ans
 
-solution=Solution()
+
+solution = Solution()
 print(solution.minimumDifference([9, 4, 1, 7], 3))
