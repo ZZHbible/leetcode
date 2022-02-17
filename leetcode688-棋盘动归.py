2767a296d732f94d4d7038d06611d8befff59003
zhangzhenhua <804459116@qq.com>
2022-02-17 13:05:22 +0800

    leetcode688-棋盘动归

--- leetcode688-棋盘动归.py
+++ leetcode688-棋盘动归.py
@@ -1,6 +1,6 @@
 class Solution:
     def knightProbability(self, n: int, k: int, row: int, column: int) -> float:
-        dp = [[[0] * n for i in range(n)] for j in range(k+1)]
+        dp = [[[0] * n for i in range(n)] for j in range(k + 1)]
         for i in range(n):
             for j in range(n):
                 dp[0][i][j] = 1
@@ -9,10 +9,12 @@ class Solution:
             for i in range(n):
                 for j in range(n):
                     for direction in directions:
-                        if i + direction[0] >= 0 and i + direction[0] < n and j + direction[1] >= 0 and j + direction[1] < n:
+                        if i + direction[0] >= 0 and i + direction[0] < n and j + direction[1] >= 0 and j + direction[
+                            1] < n:
                             dp[step][i][j] += 1 / 8 * dp[step - 1][i + direction[0]][j + direction[1]]
 
         return dp[k][row][column]
 
-solution=Solution()
+
+solution = Solution()
 print(solution.knightProbability(3, 2, 0, 0))
