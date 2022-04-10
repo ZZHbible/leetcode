88d41d9aa53c9729e8efbc16ad87cf6c4fd84489
804459116@qq.com <804459116@qq.com>
2022-04-10 11:48:49 +0800

    leetcode135-分发糖果

--- leetcode135-分发糖果.py
+++ leetcode135-分发糖果.py
@@ -31,5 +31,25 @@ class Solution:
                 dp[i] = dp[i + 1] + 1 if i + 1 < len(dp) else 1
 
         digui(0, dp)
-        print(dp)
         return sum(dp)
+
+    # 左右 两遍遍历
+    def candy_ans(self, ratings: List[int]) -> int:
+        n = len(ratings)
+        left = [0] * n
+        for i in range(n):
+            if i > 0 and ratings[i] > ratings[i - 1]:
+                left[i] = left[i - 1] + 1
+            else:
+                left[i] = 1
+
+        right = ret = 0
+        for i in range(n - 1, -1, -1):
+            if i < n - 1 and ratings[i] > ratings[i + 1]:
+                right += 1
+            else:
+                right = 1
+            ret += max(left[i], right)
+
+        return ret
+
