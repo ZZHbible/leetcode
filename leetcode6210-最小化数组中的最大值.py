117e70a5e29f16276ae046335d66e3a03388694e
ZZHbible <804459116@qq.com>
2022-10-15 23:54:03 +0800

    leetcode6210-最小化数组中的最大值-二分重要！

--- leetcode6210-最小化数组中的最大值.py
+++ leetcode6210-最小化数组中的最大值.py
@@ -4,18 +4,19 @@
 # project = leetcode6210-最小化数组中的最大值
 from typing import List
 
+
 class Solution:
     def minimizeArrayValue(self, nums: List[int]) -> int:
-        _min,_max=nums[0],max(nums)
+        _min, _max = nums[0], max(nums)
         while _min < _max:
-            mid=_min+((_max-_min)>>1)
-            temp=0
+            mid = _min + ((_max - _min) >> 1)
+            temp = 0
             for num in nums:
-                temp+=(mid-num)
+                temp += (mid - num)
                 if temp < 0:
                     break
             if temp < 0:
-                _min=mid+1
+                _min = mid + 1
             else:
-                _max=mid
+                _max = mid
         return _max
