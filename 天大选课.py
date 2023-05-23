81f6070edd667d7de357dac5c1b552498f7324f9
ZZHbible <804459116@qq.com>
2023-05-23 20:06:49 +0800

    Add verification password

--- 天大选课.py
+++ 天大选课.py
@@ -3,6 +3,7 @@ import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
+import ddddocr
 
 url = "http://classes.tju.edu.cn/eams/homeExt.action"
 
@@ -17,7 +18,14 @@ with open('./class.json') as f:
 
 driver.find_element(by=By.ID, value="username").send_keys(id)
 driver.find_element(by=By.ID, value="password").send_keys(pwd)
-driver.implicitly_wait(8)
+image = driver.find_element(by=By.ID, value="captcha_img")
+image.screenshot("code.png")
+ocr=ddddocr.DdddOcr()
+with open("code.png",'rb') as f:
+    image=f.read()
+catch = ocr.classification(image)
+driver.find_element(by=By.ID, value='captcha').send_keys(catch)
+sleep(8)
 
 driver.find_element(by=By.CLASS_NAME, value="btn-submit").click()
 
@@ -31,16 +39,16 @@ driver.implicitly_wait(10)
 driver.find_element(by=By.CLASS_NAME, value='electTabSelected').click()
 while True:
     driver.find_element(by=By.XPATH, value='//*[@id="electableLessonList"]/thead/tr[1]/th[2]/div/input').send_keys(
-        "06135")
+        "04914")
     driver.find_element(by=By.XPATH, value='//*[@id="electableLessonList"]/thead/tr[1]/th[3]/div/input').send_keys(
-        "S4028001")
+        "S212E037")
     driver.find_element(by=By.XPATH, value='//*[@id="electableLessonList"]/thead/tr[1]/th[4]/div/input').send_keys(
-        "信息检索与分析")
+        "积极心理学")
     driver.find_element(by=By.XPATH, value='//*[@id="electableLessonList_filter_submit"]').click()
     sleep(0.5)
     try:
-        if driver.find_element(by=By.XPATH, value='//*[@id="lesson424820"]/td[12]/a').text == "选课":
-            driver.find_element(by=By.XPATH, value='//*[@id="lesson424820"]/td[12]/a').click()
+        if driver.find_element(by=By.XPATH, value='//*[@id="lesson431810"]/td[12]/a').text == "选课":
+            driver.find_element(by=By.XPATH, value='//*[@id="lesson431810"]/td[12]/a').click()
             driver.switch_to.alert.accept()
     except:
         print("hello")
